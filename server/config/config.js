01fd32059b0e5b8680a9ffeb1527aeeeb6821f5b
bikeredu <luiseduardo.tellez@gmail.com>
2019-07-31 15:45:49 -0500

    Cambio variable entorno HEROKU

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 // if (process.env.NODE_ENV === 'dev') {
 //     urlDB = 'mongodb://localhost:27017/cafe';
 // } else {
-urlDB = 'mongodb+srv://bikeredu:@eT358114@cluster0-em0dw.mongodb.net/cafe'
-    //}
+urlDB = process.env.MONGO_URI;
+//}
 
 process.env.URLDB = urlDB;
